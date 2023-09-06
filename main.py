23ca31e4aef565bf8dc576890a0743ea422fdd5a
Name <user@example.com>
2023-09-06 16:33:19 +0200

    Add additional info.

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ with col1:
     st.image("images/photo.png")
 
 with col2:
-    st.title("Rafał Jakubowski")
+    st.title("Ardit Sulce")
     content = """
     Hi, I am Ardit! I am a Python programmer, teacher, and founder of PythonHow. I graduated in 2013 with a Master of Science in Geospatial Technologies from the University of Muenster in Germany with a focus on using Python for remote sensing.
     I have worked with companies from various countries, such as the Center for Conservation Geography, to map and understand Australian ecosystems, image processing with the Swiss in-Terra, and performing data mining to gain business insights with the Australian Rapid Intelligence.
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me.
+"""
+
+st.write(content2)
+
