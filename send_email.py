1546b9fee1d7b77ed91bb20fbc327cc968d71992
Name <user@example.com>
2023-09-12 18:13:39 +0200

    End of app

--- send_email.py
+++ send_email.py
@@ -8,6 +8,7 @@ def send_email(message):
     context = ssl.create_default_context()
     receiver = "jakuborafal@gmail.com"
 
+
     with smtplib.SMTP_SSL(host, port, context=context) as server:
         server.login(username, password)
         server.sendmail(username, receiver, message)
